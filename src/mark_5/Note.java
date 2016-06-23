962ec486fc107f0abbe20b931310114a6f01a157
gbtn87 <biten190@hotmail.com>
2016-06-23 10:41:24 -0300

    deleted unusefull stuff

--- src/mark_5/Note.java
+++ src/mark_5/Note.java
@@ -8,33 +8,28 @@ package mark_5;
 import java.awt.Image;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
  * @author Gustavo
  */
-class Note
+final class Note
 {    
-    static ArrayList<String> notes = fillArray();
-    String name;
-    int pos;
-    Image image;
-    Point noteLoc;
+    public static ArrayList<String> notes = fillArray();
+    public String name;
+    public int pos;
+    public Point noteLoc;
     
-    /**Método informa onde será posicionada a imagem da "whole-note" no staff
+    /**
+     * Método informa onde será posicionada a imagem da "whole-note" no staff
      * o método usa a posição da nota no array e a multiplica por um fator
      * para posicioná-la no lugar adequado
      * Assim, não é preciso assinalar manualmente a posição para cada nota
     */
     public void setNoteLoc()
     {
-        for(String noteName : notes)
-        {
-            if (noteName.toUpperCase().equals(name.toUpperCase())) 
-            {
-                noteLoc = new Point(100, (110-(12*pos)));
-            }
-        }
+        noteLoc = new Point(100, (110-(12*pos)));
     }
     
     /**
@@ -46,10 +41,6 @@ class Note
         return noteLoc;
     }
     
-    /**
-     * Construtor: toda vez que eu criar uma nova nota, ele executa esses passos
-     * @param note 
-     */
     Note(String note)
     {
         name = note;
@@ -57,13 +48,16 @@ class Note
         setNoteLoc();
     }
     
+    /**
+     * Basicamente inicializa um ArrayList de String[s]
+     * convertendo um Array em ArrayList
+     * @return 
+     */
     public static ArrayList<String> fillArray()
     {
-        String[] notesA = {"D","E","F","G","A","B","C","Dh","Eh","Fh","Gh"};
-        ArrayList<String> array = new ArrayList<String>();
-        for (int i = 0; i < notesA.length; i++) {
-            array.add(notesA[i]);
-        }
+        String[] notesArray = {"D","E","F","G","A","B","C","Dh","Eh","Fh","Gh"};
+        ArrayList<String> array = new ArrayList();
+        array.addAll(Arrays.asList(notesArray));
         return array;
     }
 }
